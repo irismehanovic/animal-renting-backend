954b72112082e1e26088676534662a515d69220f
mefnan <merdanefnan@gmail.com>
2022-04-24 18:55:54 +0200

    implementing controllers and services and 5 mapping methods

---
+++ src/main/java/com/animalrenting/AnimalRentingApplication.java
@@ -0,0 +1,13 @@
+package com.animalrenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalRentingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animalrenting/AnimalRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalrenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
