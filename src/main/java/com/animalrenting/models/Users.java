8f35b58cb390999649fa8bf09cecbcd2e00a2398
irismehanovic <iris.mehanovic@stu.ssst.edu.ba>
2022-05-04 20:56:11 +0200

    Adding user backend postgreSQL

--- src/main/java/com/animalrenting/models/Users.java
+++ src/main/java/com/animalrenting/models/Users.java
@@ -1,21 +1,53 @@
 package com.animalrenting.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 
 public class Users {
-      private long ID;
-      private String username;
-      private String name;
-      private String lastName;
-      private String email;
-      private String password;
-      private City city;
-      private String address;
-      private UserType userType;
-      private int phoneNumber;
-      private int dateOfBirth;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "city")
+    @Enumerated(value = EnumType.STRING)
+    private City city;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "user_type")
+    @Enumerated(value = EnumType.STRING)
+    private UserType userType;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private int dateOfBirth;
 }
