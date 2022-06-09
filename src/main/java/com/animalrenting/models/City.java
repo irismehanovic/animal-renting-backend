ead09c50e72426c83ccc2e8ecb8b1080f2e8c55e
mefnan <merdanefnan@gmail.com>
2022-06-09 21:02:17 +0200

    make it better

--- src/main/java/com/animalrenting/models/City.java
+++ src/main/java/com/animalrenting/models/City.java
@@ -19,3 +19,33 @@ public enum City {
     Konjic,
     Lukavac,
 }
+
+
+//package com.hero.controllers;
+//
+//        import com.hero.util.JwtUtil;
+//        import org.springframework.web.bind.annotation.GetMapping;
+//        import org.springframework.web.bind.annotation.RequestHeader;
+//        import org.springframework.web.bind.annotation.RequestMapping;
+//        import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/user")
+//public class UserController {
+//
+//
+//    private final JwtUtil jwtTokenUtil;
+//
+//    public UserController(JwtUtil jwtTokenUtil) {
+//        this.jwtTokenUtil = jwtTokenUtil;
+//    }
+//
+//    @GetMapping("/whoami")
+//    public String getUser(@RequestHeader(name = "Authorization") String token) {
+//        String username = jwtTokenUtil.extractUsername(token.substring(7));
+//        // get user from the DB by this username from token
+//        // and return the user as a DTO to the FE
+//        return username;
+//    }
+//
+//}
